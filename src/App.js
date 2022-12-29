d279c762bec138e6b037ae3771b6aa2767d9ed3d
raketej <jennifer.rakete@exxeta.com>
2022-12-29 18:21:30 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import FeedbackStats from './components/FeedbackStats';
@@ -5,6 +6,7 @@ import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackForm from './components/FeedbackForm';
+import About from './pages/About';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,14 +23,25 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+              </>
+            }
+          ></Route>
+          <Route path='/about' element={<About />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
