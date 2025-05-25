6b113c74c2f6f58f6302f170eb22048cdd906da6
jr63vuga <jr63vuga@studserv.uni-leipzig.de>
2025-05-25 13:02:57 +0200

    improved rating ui

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,5 +1,5 @@
-import { useState, useContext, useEffect } from 'react';
-import FeedbackContext from '../context/FeedbackContext';
+import { useState, useContext, useEffect } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
 function RatingSelect({ select }) {
   const [selected, setSelected] = useState(10);
@@ -16,18 +16,25 @@ function RatingSelect({ select }) {
   };
 
   return (
-    <ul className='rating'>
+    <ul className="rating">
       {Array.from({ length: 10 }, (_, i) => (
         <li key={`rating-${i + 1}`}>
           <input
-            type='radio'
+            type="radio"
             id={`num${i + 1}`}
-            name='rating'
+            name="rating"
             value={i + 1}
             onChange={handleChange}
             checked={selected === i + 1}
           />
-          <label htmlFor={`num${i + 1}`}>{i + 1}</label>
+          <label
+            style={
+              i + 1 <= selected ? { background: "#ff6a95", color: "#fff" } : {}
+            }
+            htmlFor={`num${i + 1}`}
+          >
+            {i + 1}
+          </label>
         </li>
       ))}
     </ul>
